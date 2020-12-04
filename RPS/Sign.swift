0da1008872b463171544bf823b394a92fea4224f
Julien Eyries <julien@eyries.fr>
2020-12-04 12:23:51 +0100

    added GameState & Sign

---
+++ RPS/Sign.swift
@@ -0,0 +1,57 @@
+//
+//  Sign.swift
+//  RPS
+//
+//  Created by Julien Eyriès on 04/12/2020.
+//  Copyright © 2020 Matthew Hanlon. All rights reserved.
+//
+
+import Foundation
+
+func randomSign() -> Sign {
+    let number = Int.random(in: 0 ... 2)
+    if number == 0 {
+        return .rock
+    } else if number == 1 {
+        return .paper
+    } else {
+        return .scissors
+    }
+}
+
+enum Sign {
+    case rock
+    case paper
+    case scissors
+    
+    var emoji: String {
+        switch self {
+        case .rock:
+            return "👊"
+        case .paper:
+            return "🖐"
+        case .scissors:
+            return "✌️"
+        }
+    }
+    
+    func gameState(againt opponentSign: Sign) -> GameState {
+        if opponentSign == self {
+            return .draw
+        }
+        
+        if self == .rock && opponentSign == .scissors {
+            return .win
+        }
+        
+        if self == .paper && opponentSign == .rock {
+            return .win
+        }
+        
+        if self == .scissors && opponentSign == .paper {
+            return .win
+        }
+        
+        return .lose
+    }
+}
