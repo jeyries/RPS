fec3c7ea63bcfc4c796077ddc16fffc31f5b2eef
Julien Eyries <julien@eyries.fr>
2020-12-04 12:31:38 +0100

    switching to SwiftUI

---
+++ RPS/RootView.swift
@@ -0,0 +1,21 @@
+//
+//  RootView.swift
+//  RPS
+//
+//  Created by Julien Eyriès on 04/12/2020.
+//  Copyright © 2020 Matthew Hanlon. All rights reserved.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
